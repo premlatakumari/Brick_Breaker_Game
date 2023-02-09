8fb9c136a2098b56dbf258aac1854756224b13f9
Premlata Kumari <someesingh992@gmail.com>
2023-02-09 14:33:34 +0530

    First Commit

--- src/DemoGame/GamePlay.java
+++ src/DemoGame/GamePlay.java
@@ -1,4 +1,75 @@
 package DemoGame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class GamePlay {
+public class GamePlay extends JPanel implements ActionListener, KeyListener {
+    private boolean play=false;
+    private int totalBrick=21;
+    private Timer timer;
+    private int delay=8;
+    private int ballposX=120;
+    private int ballposY=350;
+    private int ballXdir=-1;
+    private int ballYdir=-2;
+    private int playerX=350;
+    public GamePlay(){
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(true);
+        timer=new Timer(delay,this);
+        timer.start();
+
+
+    }
+    public void paint(Graphics g)
+    {
+        //black canvas
+        g.setColor(Color.black);
+        g.fillRect(1,1,692,592);
+
+        //border
+        g.setColor(Color.yellow);
+        g.fillRect(0,0,692,3);
+        g.fillRect(0,3,3,3);
+        g.fillRect(691,3,3,592);
+
+        //paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX,550,100,8);
+
+        //Ball
+        g.setColor(Color.red);
+        g.fillOval(ballposX,ballposY,20,20);
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent args){
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+
 }
--- src/DemoGame/MainClass.java
+++ src/DemoGame/MainClass.java
@@ -1,4 +1,18 @@
 package DemoGame;
-
+//to make game i need to use JFrame here i am importing
+import  javax.swing.JFrame;
 public class MainClass {
+    public static void main(String[] args) {
+        //making object of JFrame
+        JFrame f=new JFrame();
+        GamePlay gamePlay=new GamePlay();
+        f.setTitle("Brick Breaker");
+        f.setSize(700,600);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+        f.setResizable(false);
+        f.add(gamePlay);
+
+    }
 }
